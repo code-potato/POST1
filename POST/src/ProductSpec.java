532d50838795ff0a4f874bd326e2ec2373354c01
David D. Hua <dhd12391@gmail.com>
2014-02-10 18:28:29 -0800

    worked on product catalog and product specs

---
+++ POST/src/ProductSpec.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David
+ */
+public class ProductSpec {
+    private String UPC;
+    private String description;
+    private float price;
+    
+    public ProductSpec(){
+    }
+    
+    public void setUPC(String UPC){ 
+        UPC = this.UPC;
+    }
+    
+    public void setDescription(String description){
+        description = this.description;
+    }
+    
+    public void setPrice(float price){
+        price = this.price;
+    }
+    
+    
+    
+}

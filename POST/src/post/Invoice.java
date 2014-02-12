162475798084f626f2f60ca7cc57aea5375d21d7
kungfulucky7 <terrywong283@live.com>
2014-02-12 00:01:51 -0800

    POST 1 fixed a typo.

--- POST/src/post/Invoice.java
+++ POST/src/post/Invoice.java
@@ -3,61 +3,65 @@ package post;
 import java.util.ArrayList;
 
 /**
- * Invoice handles how transactions are presented. 
+ * Invoice handles how transactions are presented.
+ *
  * @author Michael Santer
  */
 public class Invoice {
+
     TransactionRecord transaction;
     Double total, amountReturned;
     Store store;
-    
-    public Invoice(TransactionRecord transaction,double total, Store store){
+
+    public Invoice(TransactionRecord transaction, double total, Store store) {
         this.transaction = transaction;
-        this.store= store;
+        this.store = store;
         this.total = total;
     }
-    
+
     /**
      * Constructs the layout of a plain text invoice.
+     *
      * @return String
      */
-    public String toString(){
+    public String toString() {
         String invoice = "";
         invoice += "______________________________________________________\n\n";
         invoice += store.getName() + "\n";
         invoice += "______________________________________________________\n\n";
-        
+
         invoice += String.format("%-25s %-20s\n", "Customer Name:", "Date & Time:");
-        
+
         String customerName = transaction.getCustomerFirstName();
-        if(transaction.getCustomerLastName() != null)
+        if (transaction.getCustomerLastName() != null) {
             customerName += " " + transaction.getCustomerLastName();
-        
-        invoice += String.format("%-25s %-20s\n\n", customerName, 
-                                transaction.getDateAndTime());
-        invoice += String.format("%-12s %-12s %-12s %-12s\n", "Item:", "QTY:", 
-                                "Unit Price:", "Subtotal:");
-        
-        ArrayList<Item> items = transaction.getItems();
-        for(Item item : items){
-            double price = store.getProduct(item.getUPC()).getPrice();
-            invoice += String.format("%-12s %-12s $%-12.2f $%-12.2f\n", 
-                    store.getProduct(item.getUPC()).getDescription(), 
-                    item.getQuantity(), price, price*item.getQuantity());
         }
-        
+
+        invoice += String.format("%-25s %-20s\n\n", customerName,
+                transaction.getDateAndTime());
+        invoice += String.format("%-12s %-12s %-12s %-12s\n", "Item:", "QTY:",
+                "Unit Price:", "Subtotal:");
+
+        ArrayList<Item> items = transaction.getItems();
+        for (Item item : items) {
+            double price = store.getProduct(item.getUPC()).getPrice();
+            invoice += String.format("%-12s %-12s $%-12.2f $%-12.2f\n",
+                    store.getProduct(item.getUPC()).getDescription(),
+                    item.getQuantity(), price, price * item.getQuantity());
+        }
+
         invoice += "------------------------------------------------------\n";
         invoice += String.format("Total: $%.2f\n", total);
 
-        if(transaction.getPayment() instanceof CashPayment)
-            invoice += "Amount Tendered: $" + 
-                        String.format("%.2f\n",transaction.getPayment().getAmount()) 
-                    + "Amount Returned: $" + 
-                        String.format("%.2f\n", amountReturned);
-        else if(transaction.getPayment() instanceof CheckPayment)
-            invoice += "Piad by check \n";
-        else{ //if credit card
-            CreditPayment credit = (CreditPayment)transaction.getPayment();
+        if (transaction.getPayment() instanceof CashPayment) {
+            invoice += "Amount Tendered: $"
+                    + String.format("%.2f\n", transaction.getPayment().getAmount())
+                    + "Amount Returned: $"
+                    + String.format("%.2f\n", amountReturned);
+        } else if (transaction.getPayment() instanceof CheckPayment) {
+            invoice += "Paid by Check \n";
+        } else { //if credit card
+            CreditPayment credit = (CreditPayment) transaction.getPayment();
             invoice += "Paid by Credit Card " + credit.getAccountNumber() + "\n";
         }
         invoice += "\n******************************************************\n\n";
@@ -65,7 +69,7 @@ public class Invoice {
     }
 
     /**
-     * Prints invoice to terminal. 
+     * Prints invoice to terminal.
      */
     public void print() {
         System.out.println(this);
@@ -74,8 +78,4 @@ public class Invoice {
     void setAmountReturned(double amount) {
         amountReturned = amount;
     }
-    
-    
-    
-    
 }
